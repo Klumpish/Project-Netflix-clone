301cd8e24fba10958b1022d314b351400f229397
Henrik <Henrikmattsson89@gmail.com>
2025-07-08 03:43:24 +0200

    TitleCards are working and maping data,scroll function added on titleCards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
 
-const TitleCards = () => {
-	return <div className="titlecards">TitleCards</div>;
+const TitleCards = ({ title, category }) => {
+	// enables scrolling in the to the left if we use our scroll on the moive cards
+	const cardsRef = useRef();
+	const handleWheel = (e) => {
+		e.preventDefault();
+		cardsRef.current.scrollLeft += e.deltaY;
+	};
+
+	useEffect(() => {
+		cardsRef.current.addEventListener('wheel', handleWheel);
+	}, []);
+
+	return (
+		<div className="title-cards">
+			<h2>{title ? title : 'Popular on Netflix'}</h2>
+			<div
+				className="card-list"
+				ref={cardsRef}>
+				{cards_data.map((card, index) => (
+					<div
+						className="card"
+						key={index}>
+						<img
+							src={card.image}
+							alt={'Image of movie ' + card.name}
+						/>
+						<p>{card.name}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default TitleCards;
